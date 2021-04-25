eebb23bae5be5246301af4d74a6adf81e0184c86
Carlos Gallo <carlos.gallo@somagrupo.com.br>
2021-04-25 02:32:08 -0300

    upgrading boilerplate

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,16 @@
+import * as S from './styles'
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="logo em forma de atomo e React AVançado escrito ao lado"
+    />
+    <S.Title> React Avançado</S.Title>
+    <S.Description>Typescript, React JS</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="um desenvolvedor de frente para um computador"
+    />
+  </S.Wrapper>
+)
+export default Main
